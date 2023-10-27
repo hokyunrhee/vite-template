4805ec8fe7c6368d9a31f955edcd1b5bbb4e5b66
hokyunrhee <hokyun.rhee@gmail.com>
2023-10-27 23:00:32 +0900

    build: eslint, prettier 추가

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  printWidth: 120,
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrderSeparation: false,
+  importOrder: ["<BUILTIN_MODULES>", "", "<THIRD_PARTY_MODULES>", "", "^@", "^[./]"],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+}
