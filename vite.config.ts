925398a8c82bafc9902ec4bef505eb11dcf9ae9a
hokyunrhee <hokyun.rhee@gmail.com>
2023-10-29 10:17:41 +0900

    build: rollup-plugin-visualizer 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc"
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), visualizer()],
 })
